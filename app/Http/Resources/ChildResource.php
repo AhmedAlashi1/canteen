5389a91c565697434409fb9e263de4ddcc5e5faa
AhmedAlashi1 <aalshy00@gmail.com>
2025-05-18 18:02:53 +0300

    update api

--- app/Http/Resources/ChildResource.php
+++ app/Http/Resources/ChildResource.php
@@ -16,14 +16,14 @@ class ChildResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'level' => $this->level_name,
-            'level_id' => $this->level_id,
-            'student_number' => $this->student_number,
-            'image' => url($this->image),
-            'status' => $this->status,
-            'total_order_days' => $this->total_order_days,
-            'orders' => $this->orders ?? [],
+//            'name' => $this->name,
+//            'level' => $this->level_name,
+//            'level_id' => $this->level_id,
+//            'student_number' => $this->student_number,
+//            'image' => url($this->image),
+//            'status' => $this->status,
+//            'total_order_days' => $this->total_order_days,
+//            'orders' => $this->orders ?? [],
 //            'school' => new SchoolResource($this->whenLoaded('school')),
 //            'user' => new UserResource($this->whenLoaded('user')),
             //orderDays
