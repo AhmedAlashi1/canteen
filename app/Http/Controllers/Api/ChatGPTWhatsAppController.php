b716f0c07204f44026c7796516634597a72234c3
AhmedAlashi1 <aalshy00@gmail.com>
2025-07-23 11:05:03 +0300

    update webhook whatsapp

--- app/Http/Controllers/Api/ChatGPTWhatsAppController.php
+++ app/Http/Controllers/Api/ChatGPTWhatsAppController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class ChatGPTWhatsAppController extends Controller
 {
